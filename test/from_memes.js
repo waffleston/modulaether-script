8c9aff96bcd4b7811b606cab460c1ec1f87afe0f
Brendyn Sonntag <waffles@nawibo.com>
2017-03-14 15:29:18 -0500

    [QFE]  fixed ommitted spaces in %^thisfile

--- test/from_memes.js
+++ test/from_memes.js
@@ -1,8 +1,8 @@
 /**
  * This file generated from Modulaetherschrift source.
 **/
-console.log('scriptloaded');
-//%^srcdeffrom_memes
-functionmemesaes(){
-alert('Analerthasbeensent.');
-}
+console.log('script loaded');
+ //%^srcdef from_memes
+ function memesaes() {
+ alert('An alert has been sent.');
+ } 

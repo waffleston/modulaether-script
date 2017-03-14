47b19d57e239ca052c0ca958dd5faef9b146dfd0
Brendyn Sonntag <waffles@nawibo.com>
2017-03-14 06:50:08 -0500

    Created test directory

---
+++ test/fileone.js
@@ -0,0 +1,13 @@
+/**
+ * This file generated from Modulaetherschrift source.
+**/
+function maes_selectMemes(one,two) {
+ //Let's figure out what the memes are.
+ console.log("one: ", one);
+ console.log("two: ", two);
+ var aboi = {
+ hi: 0,
+ ne: 1
+ }
+ }
+ 
---
+++ test/nicer.js
@@ -0,0 +1,4 @@
+/**
+ * This file generated from Modulaetherschrift source.
+**/
+function maes_hello(nice) { console.log(nice) } 
